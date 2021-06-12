db5abf97021da0285f3e6f349532f8a38b942b57
hendryfoe <hendryanto.fudiko@gmail.com>
2021-06-12 15:58:03 +0700

    init project
    - with typescript
    - with tailwind
    - with prettier

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto py-10 h-screen">
+      <h2 className="text-lg font-bold">Hello World</h2>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+}
